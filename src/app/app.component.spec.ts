193711182e9a6bf81ee4e9a03e6c1902613abdbc
Shravani  Manisha Manoj Kaleshwarkar <65099540+Shravanikalesh344@users.noreply.github.com>
2023-12-30 20:24:31 +0530

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'A6P4' title`, () => {
+  it(`should have the 'A7P3' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('A6P4');
+    expect(app.title).toEqual('A7P3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, A6P4');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, A7P3');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NewCompComponent } from './new-comp/new-comp.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NewCompComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'A6P4';
+  
+   public name : string = "";
+   public fun()
+   {
+         this.name  = "Educating for better tomrrorow";
+   }
+
+   
 }
