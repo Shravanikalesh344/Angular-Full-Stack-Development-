6c7c585bae56c4c554cf580c559c09b91699d507
Shravani  Manisha Manoj Kaleshwarkar <65099540+Shravanikalesh344@users.noreply.github.com>
2023-12-30 20:19:42 +0530

    Add files via upload

---
+++ src/app/new-comp/new-comp.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-comp.component.html',
+  styleUrl: './new-comp.component.css'
+})
+export class NewCompComponent {
+
+    public name : string = "Marvellous";
+
+   public concat : string = "Infosystems";
+
+}
